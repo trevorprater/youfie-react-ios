986e7a8619ca690c5f878a9de66140ef48b9e9c8
Trevor Prater <trevor.prater@enigma.io>
2016-12-22 02:10:36 -0500

    Adds firebase config

--- index.ios.js
+++ index.ios.js
@@ -24,6 +24,14 @@ var NoNavigatorPage = require('./components/NoNavigatorPage');
 
 const styles = require('./styles.js');
 
+const firebaseConfig = {
+    apiKey: "AIzaSyDqk1DhYw2PfDVpU9NPPf5BZrCWHJ2lVIo",
+    authDomain: "youfie-983ce.firebaseapp.com",
+    databaseURL: "https://youfie-983ce.firebaseio.com",
+    storageBucket: "youfie-983ce.appspot.com",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
 class Youfie extends Component {
   render() {
     return (
