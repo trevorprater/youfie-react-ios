7f6a260265f40b7bb63b767a6f5c33b840430057
Trevor Prater <trevor.prater@enigma.io>
2017-01-07 21:41:41 -0500

    Adds constants

---
+++ constants.js
@@ -0,0 +1,8 @@
+const firebaseConfig = { 
+            apiKey: "AIzaSyDqk1DhYw2PfDVpU9NPPf5BZrCWHJ2lVIo",
+            authDomain: "youfie-983ce.firebaseapp.com",
+            databaseURL: "https://youfie-983ce.firebaseio.com",
+            storageBucket: "youfie-983ce.appspot.com",
+}
+
+module.exports.firebaseConfig = firebaseConfig
